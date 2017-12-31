d9baf5277ab1d99ca907548f7a5edda92297acc1
pcarr <pat@tuxengineering.com>
2017-12-31 14:53:30 -0800

    Fix hang when number of slaves is >2
    Remove Serial output in callback OnDataSent().

--- Master/Master.ino
+++ Master/Master.ino
@@ -50,7 +50,7 @@
 #include <WiFi.h>
 
 // Global copy of slave
-#define NUMSLAVES 20
+#define NUMSLAVES 5
 esp_now_peer_info_t slaves[NUMSLAVES] = {};
 int SlaveCnt = 0;
 int scan_delay = 0;
@@ -199,17 +199,17 @@ void sendData(uint8_t data) {
     } else {
       Serial.println("Not sure what happened");
     }
-    delay(100);
+    delay(200);
   }
 }
 
 // callback when data is sent from Master to Slave
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
-  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  //snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+  //         mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  //Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+  //Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup() {
